d0b1a26726cafc0fd88dea2fe30202b7c0cc9116
Ernesto Rojo <erojo@ernestorojo.com>
2026-03-02 09:38:39 -0600

    Updated ArtistDetailPage to receive an ArtistModel

--- src/Pages/ArtistDetailPage.xaml.cs
+++ src/Pages/ArtistDetailPage.xaml.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 using MusicApp.ViewModels;
 
 namespace MusicApp.Pages;
 
+[QueryProperty("Artist", "model")]
 public partial class ArtistDetailPage : ContentPage
 {
     public ArtistDetailPage(ArtistDetailViewModel viewModel)
--- src/ViewModels/ArtistDetailViewModel.cs
+++ src/ViewModels/ArtistDetailViewModel.cs
@@ -1,6 +1,19 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MusicApp.Models;
+
 namespace MusicApp.ViewModels;
 
-public class ArtistDetailViewModel
+[QueryProperty("Artist", "model")]
+public partial class ArtistDetailViewModel : ObservableObject
 {
-    
+    [ObservableProperty]
+    private ArtistModel _artist;
+
+    [RelayCommand]
+    async Task OnGoBack()
+    {
+        await Shell.Current.GoToAsync("..");
+        
+    }
 }
